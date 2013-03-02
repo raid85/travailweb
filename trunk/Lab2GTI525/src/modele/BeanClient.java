885282f14accaa57c27109a1332ec16b5af10158
abmarinescu@gmail.com <abmarinescu@gmail.com@10bb887d-6365-b616-f229-12e7dcfb17ad>
2013-03-02 00:16:54 +0000

    Création des Beans et du DAO des Spectacles, ne'est pas encore relié, prochaine étaape est de popules la page spectacles, jsp à l'aide d'un BeanSpectacle, on doit également créer les Stubs(d'images etc.)

---
+++ trunk/Lab2GTI525/src/modele/BeanClient.java
@@ -0,0 +1,20 @@
+package modele;
+
+public class BeanClient {
+
+	private int id = -1;
+	private String nomFamille = null;
+	private String prenom = null;
+	private String courriel = null;
+	private String tel = null;
+	private String anniv = null;
+	private String adresse = null;
+	private String ville = null;
+	private String province = null;
+	private String codePostal = null;
+	private String motDePasse = null;
+	private String forfaitID = null ;
+	private int creditID = -1 ;
+	
+	
+}
