f8641dcba4ca0d2f192b16a76683815b812e009d
esber.hani <esber.hani@10bb887d-6365-b616-f229-12e7dcfb17ad>
2013-04-02 02:24:33 +0000

    infos des beans sont toutes dans la BD maintenant et ca marche :)

--- trunk/Lab2GTI525/src/modele/SpectacleDAO.java
+++ trunk/Lab2GTI525/src/modele/SpectacleDAO.java
@@ -13,10 +13,10 @@ public class SpectacleDAO {
 	//FIXME: Ceci devrait [etre fourni dans un config file et no pas harcode
 	public static final String driver = "org.sqlite.JDBC";
 	//adresse de la bd locale
-	private static String url = "jdbc:sqlite:F:/EclipseWorkspace/Lab2GTI525/BD/bd.sqlite";
-	//private static String url = "jdbc:sqlite:C:/Users/Hani/workspace/Lab2GTI525/BD/bd.sqlite";
+	//private static String url = "jdbc:sqlite:F:/EclipseWorkspace/Lab2GTI525/BD/bd.sqlite";
+	private static String url = "jdbc:sqlite:C:/Users/Hani/workspace/Lab2GTI525/BD/bd.sqlite";
 	private static ResultSet resultatBD;
-	
+
 	private BeanSpectacle beanSpectacle ;
 	private BeanRepresentation beanRepresentation;
 
@@ -38,19 +38,19 @@ public class SpectacleDAO {
 	//Retourne le resultat d<une requ[ete de type sqllite
 	//Ouvre l'acces a la bd locale.
 	private static ResultSet sqlQuerry(String sqlRequest){
-		
+
 		ResultSet resSet = null;
-		
+
 		try{
-		//Load the JDBC driver class dynamically.
-		Class.forName(driver);
-		System.out.println("Driver Enregistre");
-		Connection con = DriverManager.getConnection(url);
-		System.out.println("Connection etablie");
-		Statement stmt = con.createStatement();
-		stmt.close();
-		resSet = stmt.executeQuery(sqlRequest);
-		return 	resSet;
+			//Load the JDBC driver class dynamically.
+			Class.forName(driver);
+			//System.out.println("Driver Enregistre");
+			Connection con = DriverManager.getConnection(url);
+			//System.out.println("Connection etablie");
+			Statement stmt = con.createStatement(ResultSet.TYPE_FORWARD_ONLY,ResultSet.CONCUR_READ_ONLY,ResultSet.CLOSE_CURSORS_AT_COMMIT);
+			stmt.close();
+			resSet = stmt.executeQuery(sqlRequest);
+			return 	resSet;
 		}catch(SQLException sqlE){ sqlE.printStackTrace();}
 		catch(ClassNotFoundException cE){cE.printStackTrace();}
 		try {
@@ -59,9 +59,9 @@ public class SpectacleDAO {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		return 	resSet;
-		
+
 	}
 
 
@@ -73,121 +73,137 @@ public class SpectacleDAO {
 
 	private void populateSpectacles() throws ClassNotFoundException, SQLException {
 
-		int nbSpectacles = sqlQuerry("select count (*) from spectacle").getInt(1);
-		System.out.println("TEST HANI "+nbSpectacles);
-		
-		for (int i=0; i<1; i++){
-			beanRepresentation = new BeanRepresentation();
-			beanRepresentation.setId(sqlQuerry("select id from representation where representation.id="+(i+1)).getInt(1));
-			beanRepresentation.setBilletsDispo(sqlQuerry("select billets from representation where representation.id="+(i+1)).getInt(1));
-			beanRepresentation.setNomSpectacle(sqlQuerry("select nom from representation where representation.id="+(i+1)).getString(1));
-			beanRepresentation.setPrix(sqlQuerry("select prix from representation where representation.id="+(i+1)).getInt(1));
-			beanRepresentation.setSalle(sqlQuerry("select salle from representation where representation.id="+(i+1)).getString(1));
-			beanRepresentation.setDate(sqlQuerry("select date from representation where representation.id="+(i+1)).getString(1));
-			listeRepresentations.add(beanRepresentation);
+
+		for (int j=1; j<=sqlQuerry("select count (*) from spectacle").getInt(1); j++){
+			String spec=sqlQuerry("select nom from spectacle where id="+j).getString(1);
+			resultatBD =sqlQuerry("select id from representation where representation.nom like '"+spec+"'");
+			while(resultatBD.next()){
+
+				int idREP=resultatBD.getInt(1);
+				System.out.println("idREP="+idREP);
+				beanRepresentation = new BeanRepresentation();
+				beanRepresentation.setId(sqlQuerry("select id from representation where representation.id="+idREP).getInt(1));
+				beanRepresentation.setBilletsDispo(sqlQuerry("select billets from representation where representation.id="+idREP).getInt(1));
+				beanRepresentation.setNomSpectacle(sqlQuerry("select nom from representation where representation.id="+idREP).getString(1));
+				beanRepresentation.setPrix(sqlQuerry("select prix from representation where representation.id="+idREP).getInt(1));
+				beanRepresentation.setSalle(sqlQuerry("select salle from representation where representation.id="+idREP).getString(1));
+				beanRepresentation.setDate(sqlQuerry("select date from representation where representation.id="+idREP).getString(1));
+				listeRepresentations.add(beanRepresentation);
+			}
+			beanSpectacle = new BeanSpectacle();
+			beanSpectacle.setId(sqlQuerry("select id from spectacle where spectacle.id="+j).getInt(1));
+			beanSpectacle.setNom(sqlQuerry("select nom from spectacle where spectacle.id="+j).getString(1));
+			beanSpectacle.setThumbnail(sqlQuerry("select thumbnail from spectacle where spectacle.id="+j).getString(1));
+			beanSpectacle.setPoster(sqlQuerry("select poster from spectacle where spectacle.id="+j).getString(1));
+			beanSpectacle.setDescription(sqlQuerry("select description from spectacle where spectacle.id="+j).getString(1));	
+			beanSpectacle.setListeRepresentations(listeRepresentations);
+
+			//on ajoute le bean à la liste	
+			listeSpectacles.add(beanSpectacle);
+			System.out.println("J="+j);
+			this.listeRepresentations = new ArrayList<BeanRepresentation>();
 		}
-		
-		
-		
-//		//Instanciation du Bean
-//		beanRepresentation = new BeanRepresentation();
-//
-//		//Création du Bean
-//		beanRepresentation.setId(2);
-//		beanRepresentation.setBilletsDispo(162);
-//		beanRepresentation.setNomSpectacle("Georges St-Pierre");
-//		beanRepresentation.setPrix(100);
-//		beanRepresentation.setSalle("Bruno Payette Est");
-//		beanRepresentation.setDate("23 Juin 2013");
-//
-//		//on ajoute le bean à la liste	
-//		listeRepresentations.add(beanRepresentation);
-
-		//***----------------------------*******
-		//      Exemple comment accéder a la bd
-		//***----------------------------*******
-		
-//		resultatBD = sqlQuerry("SELECT Spectacles.Nom FROM Spectacles DSWHERE Spectacles.Id_Spectacle = 1");
-//		System.out.println("TRACE" + resultatBD.getString(1));   
-		
-//		try
-//		 {
-//			
-//				String fName;
-//	            while(resultatBD.next())
-//	            {
-//	                fName = resultatBD.getString(1);
-//	                
-//	                System.out.println("2222 BD ***********/////////-:" + fName);
-//	            }
-//	        }
-//	        catch(Exception e)
-//	        {
-//	            System.out.println("Error processing results: " + e.toString());
-//	        }   
-		
-		//******************************************
-		//****---------------Fin Example-----------
-		
-		
-		//Instanciation du Bean		
-		beanSpectacle = new BeanSpectacle();
-
-		//Création du Bean
-		beanSpectacle.setId(1);
-		beanSpectacle.setNom("Georges St-Pierre");
-		beanSpectacle.setThumbnail("./images/templatemo_image_03c.jpg");
-		beanSpectacle.setPoster("./images/templatemo_image_03.jpg");
-		beanSpectacle.setDescription("Combat de Georges St-Pierre vs BJ Penn. À ne pas manquer. Lequel survivera?");	
-		beanSpectacle.setListeRepresentations(listeRepresentations);
-
-		//on ajoute le pbean à la liste	
-		listeSpectacles.add(beanSpectacle);
-
-		//ON RESET LA LISTE À Zéro pour l'utiliser pour le prochain spectacle. 
-		//Dans le prochain BeanSpectacle
-		this.listeRepresentations = new ArrayList<BeanRepresentation>();
-
-		//Instanciation du Bean
-		beanRepresentation = new BeanRepresentation();
-		//Création du Bean
-		//Representation 1
-		beanRepresentation.setId(1);
-		beanRepresentation.setBilletsDispo(234);
-		beanRepresentation.setNomSpectacle("Cirque du soleil: Amaluna");
-		beanRepresentation.setPrix(190);
-		beanRepresentation.setSalle("Ti-Guy Clément");
-		beanRepresentation.setDate("12 Mai 2013");
 
 
-		listeRepresentations.add(beanRepresentation);	
+		//		//Instanciation du Bean
+		//		beanRepresentation = new BeanRepresentation();
+		//
+		//		//Création du Bean
+		//		beanRepresentation.setId(2);
+		//		beanRepresentation.setBilletsDispo(162);
+		//		beanRepresentation.setNomSpectacle("Georges St-Pierre");
+		//		beanRepresentation.setPrix(100);
+		//		beanRepresentation.setSalle("Bruno Payette Est");
+		//		beanRepresentation.setDate("23 Juin 2013");
+		//
+		//		//on ajoute le bean à la liste	
+		//		listeRepresentations.add(beanRepresentation);
+		//
+		//		//***----------------------------*******
+		//		//      Exemple comment accéder a la bd
+		//		//***----------------------------*******
+		//		
+		//		resultatBD = sqlQuerry("SELECT Spectacles.Nom FROM Spectacles DSWHERE Spectacles.Id_Spectacle = 1");
+		//		System.out.println("TRACE" + resultatBD.getString(1));   
 
-		//Instanciation du Bean
-		beanRepresentation = new BeanRepresentation();
-		//Création du Bean
-		//Representation 2
-		beanRepresentation.setId(2);
-		beanRepresentation.setBilletsDispo(18);
-		beanRepresentation.setNomSpectacle("Cirque du soleil: Amaluna");
-		beanRepresentation.setPrix(200);
-		beanRepresentation.setSalle("Super Man");
-		beanRepresentation.setDate("14 MAI 2013");
-
-		//on ajoute le bean à la liste	
-		listeRepresentations.add(beanRepresentation);	
-
-		//Instanciation du Bean
-		beanSpectacle = new BeanSpectacle();
-		//Création du Bean
-		beanSpectacle.setId(1);
-		beanSpectacle.setNom("Cirque du soleil: Amaluna");
-		beanSpectacle.setThumbnail("./images/templatemo_image_04c.jpg");
-		beanSpectacle.setPoster("./images/templatemo_image_04b.jpg");
-		beanSpectacle.setDescription("On aime le cirque quand il y a des animaux. Mais ce n'est pas gentil pour les animaux, alors ce seront des humains qui remplaceront les singes.");	
-		beanSpectacle.setListeRepresentations(listeRepresentations);
-
-		//on ajoute le bean à la liste	
-		listeSpectacles.add(beanSpectacle);
+		//		try
+		//		 {
+		//			
+		//				String fName;
+		//	            while(resultatBD.next())
+		//	            {
+		//	                fName = resultatBD.getString(1);
+		//	                
+		//	                System.out.println("2222 BD ***********/////////-:" + fName);
+		//	            }
+		//	        }
+		//	        catch(Exception e)
+		//	        {
+		//	            System.out.println("Error processing results: " + e.toString());
+		//	        }   
+		//		
+		//		//******************************************
+		//		//****---------------Fin Example-----------
+		//		
+		//		
+		//		//Instanciation du Bean		
+		//		beanSpectacle = new BeanSpectacle();
+		//
+		//		//Création du Bean
+		//		beanSpectacle.setId(1);
+		//		beanSpectacle.setNom("Georges St-Pierre");
+		//		beanSpectacle.setThumbnail("./images/templatemo_image_03c.jpg");
+		//		beanSpectacle.setPoster("./images/templatemo_image_03.jpg");
+		//		beanSpectacle.setDescription("Combat de Georges St-Pierre vs BJ Penn. À ne pas manquer. Lequel survivera?");	
+		//		beanSpectacle.setListeRepresentations(listeRepresentations);
+		//
+		//		//on ajoute le pbean à la liste	
+		//		listeSpectacles.add(beanSpectacle);
+		//
+		//		//ON RESET LA LISTE À Zéro pour l'utiliser pour le prochain spectacle. 
+		//		//Dans le prochain BeanSpectacle
+		//		this.listeRepresentations = new ArrayList<BeanRepresentation>();
+		//
+		//		//Instanciation du Bean
+		//		beanRepresentation = new BeanRepresentation();
+		//		//Création du Bean
+		//		//Representation 1
+		//		beanRepresentation.setId(1);
+		//		beanRepresentation.setBilletsDispo(234);
+		//		beanRepresentation.setNomSpectacle("Cirque du soleil: Amaluna");
+		//		beanRepresentation.setPrix(190);
+		//		beanRepresentation.setSalle("Ti-Guy Clément");
+		//		beanRepresentation.setDate("12 Mai 2013");
+		//
+		//
+		//		listeRepresentations.add(beanRepresentation);	
+		//
+		//		//Instanciation du Bean
+		//		beanRepresentation = new BeanRepresentation();
+		//		//Création du Bean
+		//		//Representation 2
+		//		beanRepresentation.setId(2);
+		//		beanRepresentation.setBilletsDispo(18);
+		//		beanRepresentation.setNomSpectacle("Cirque du soleil: Amaluna");
+		//		beanRepresentation.setPrix(200);
+		//		beanRepresentation.setSalle("Super Man");
+		//		beanRepresentation.setDate("14 MAI 2013");
+		//
+		//		//on ajoute le bean à la liste	
+		//		listeRepresentations.add(beanRepresentation);	
+		//
+		//		//Instanciation du Bean
+		//		beanSpectacle = new BeanSpectacle();
+		//		//Création du Bean
+		//		beanSpectacle.setId(1);
+		//		beanSpectacle.setNom("Cirque du soleil: Amaluna");
+		//		beanSpectacle.setThumbnail("./images/templatemo_image_04c.jpg");
+		//		beanSpectacle.setPoster("./images/templatemo_image_04b.jpg");
+		//		beanSpectacle.setDescription("On aime le cirque quand il y a des animaux. Mais ce n'est pas gentil pour les animaux, alors ce seront des humains qui remplaceront les singes.");	
+		//		beanSpectacle.setListeRepresentations(listeRepresentations);
+		//
+		//		//on ajoute le bean à la liste	
+		//		listeSpectacles.add(beanSpectacle);
 
 		System.out.println("TRACE SpectacleDAO: " + "Liste Spectacles a été populéee");
 
